4eb0582a91ee6306a245b0317c3ea6d511263ba7
Daniel <danleavitt0@gmail.com>
2015-12-07 18:45:05 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var inherit = require('component-inherit')
+var Device = require('ev3-js-device')
+
+function Motor (path) {
+  Device.call(this, path)
+}
+
+inherit(Motor, Device)
+
+Motor.prototype.runForever = function () {
+  this.write('duty_cycle_sp', '50')
+  this.write('command', 'run-forever')
+}
+
+var motor = new Motor('/sys/class/tacho-motor/motor0')
+motor.runForever()
